c4529d41f1c23949d706d7ad1af4fcc5c9e53eb3
jessicacosta07 <jessikacostajf@gmail.com>
2021-09-11 13:49:53 -0300

    parte validacao email

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -2,8 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Button from '../Forms/Button'
 import Input from '../Forms/Input'
+import useForm from '../Hooks/useForm'
 
 const LoginForm = () => {
+    const username = useForm()
+    const password = useForm()
 
     function handleSubmit(event) {
         event.preventDefault()
@@ -25,8 +28,8 @@ const LoginForm = () => {
         <section>
             <h1>Login</h1>
             <form action='' onSubmit={handleSubmit}>
-                <Input label='Usuário' type='text' name='username' />
-                <Input label='Senha' type='´password' name='password' />
+                <Input label='Usuário' type='text' name='username' {...username} />
+                <Input label='Senha' type='password' name='password' {...password} />
                 <Button>Entrar</Button>
             </form>
             <Link to='/login/criar'>Cadastro</Link>
