1b089d03fefc74c529bb23b27dcfdd5ae4c7ed02
jessicacosta07 <jessikacostajf@gmail.com>
2021-09-11 14:11:38 -0300

    ajustes api

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -10,18 +10,21 @@ const LoginForm = () => {
 
     function handleSubmit(event) {
         event.preventDefault()
-        fetch(`https://dogsapi.origamid.dev/json/jwt-auth/v1/token`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify()
-        }).then(response => {
-            console.log(response)
-            return response.json()
-        }).then(json => {
-            console.log(json)
-        })
+
+        if (username.validate() && password.validate()) {
+            fetch(`https://dogsapi.origamid.dev/json/jwt-auth/v1/token`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify()
+            }).then(response => {
+                console.log(response)
+                return response.json()
+            }).then(json => {
+                console.log(json)
+            })
+        }
     }
 
     return (
