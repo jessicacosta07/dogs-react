32758832d42885a77946fddcfc4bf782f7cd1c69
jessicacosta07 <jessikacostajf@gmail.com>
2021-09-10 22:57:23 -0300

    parte criacao component 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
+import { Footer } from "./Components/Footer";
+import Header from "./Components/Header";
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
 
