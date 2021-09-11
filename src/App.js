a61cf08d7c6d2cb89fe3e1d035b12d9480ef5a52
jessicacosta07 <jessikacostajf@gmail.com>
2021-09-10 23:04:06 -0300

    parte components 3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import { Footer } from "./Components/Footer";
 import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
 
 function App() {
   return (
@@ -11,8 +13,8 @@ function App() {
         <Header />
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
-
         <Footer />
       </BrowserRouter>
     </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styles from './Header.module.css'
 
 const Header = () => {
     return (
         <div className={styles.header}>
-
-        </div>
+            <nav>
+                <Link to='/'>Home</Link>
+                <Link to='/login'>Login</Link>
+            </nav>
+        </div >
     )
 }
 
