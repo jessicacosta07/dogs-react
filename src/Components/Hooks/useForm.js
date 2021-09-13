4bb510c1b9895a740510bfae58accccfa3ce6229
jessicacosta07 <jessikacostajf@gmail.com>
2021-09-13 16:51:42 -0300

    parte useForm, api e loginCreate

--- src/Components/Hooks/useForm.js
+++ src/Components/Hooks/useForm.js
@@ -5,6 +5,10 @@ const types = {
         regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
         message: 'Preencha um email válido',
     },
+    password: {
+        regex: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}$/,
+        message: 'A senha precisa ter 1 caracter maiúsculo, 1 minúsculo e 1 dígito. Com no mínimo 8 caracteres'
+    }
 };
 
 const useForm = (type) => {
