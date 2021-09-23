c4f28c56554329dd0913a370fc007bd351b447e7
jessicacosta07 <jessikacostajf@gmail.com>
2021-09-23 18:32:50 -0300

    parte photo

---
+++ src/Components/Feed/FeedModal.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from './FeedModal.module.css'
+import useFetch from '../../Components/Hooks/useFetch'
+import { PHOTO_POST } from '../../api'
+import Error from '../Helper/Error'
+import Loading from '../Helper/Loading'
+import PhotoContent from '../Photo/PhotoContent'
+
+const FeedModal = ({ photo }) => {
+    const { data, error, loading, request } = useFetch()
+
+    React.useEffect(() => {
+        const { url, options } = PHOTO_POST(photo.id)
+        request(url, options)
+    }, [photo, request])
+
+    return (
+        <div className={styles.modal}>
+            {error && <Error error={error} />}
+            {loading && <Loading />}
+            {data && <PhotoContent data={data} />}
+        </div>
+    )
+}
+
+export default FeedModal
+
