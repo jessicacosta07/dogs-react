f47d33298d2683b0cc516a7fbd131a58e6dd0e0a
jessicacosta07 <jessikacostajf@gmail.com>
2021-09-23 15:45:01 -0300

    parte feedPhoto

--- src/Components/User/UserPhotoPost.js
+++ src/Components/User/UserPhotoPost.js
@@ -29,7 +29,7 @@ const UserPhotoPost = () => {
         formData.append('idade', idade.value)
 
         const token = window.localStorage.getItem('token')
-        const { url, options } = PHOTO_POST(formData)
+        const { url, options } = PHOTO_POST(formData, token)
         request(url, options)
     }
 
