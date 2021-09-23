b136e856598181546c3151d30be24f3815cc80d8
jessicacosta07 <jessikacostajf@gmail.com>
2021-09-23 11:43:47 -0300

    parte userPhoto

--- src/Components/User/UserPhotoPost.js
+++ src/Components/User/UserPhotoPost.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import styles from './UserPhotoPost.module.css'
+import Input from '../Forms/Input'
+import Button from '../Forms/Button'
 
 const UserPhotoPost = () => {
+
+    function handleSubmit(event) {
+        event.preventDefault()
+    }
+
     return (
-        <div>
-            Postar foto
-        </div>
+        <section className={`${styles.photoPost} animeLeft`}>
+            <form onSubmit={handleSubmit}>
+                <Input />
+                <Button />
+            </form>
+        </section>
     )
 }
 
