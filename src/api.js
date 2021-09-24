5b3ef188a4cabc0fa78f25917aa1cb181cc63682
jessicacosta07 <jessikacostajf@gmail.com>
2021-09-24 12:33:01 -0300

    parte photoPage

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://dogsapi.origamid.dev/json'
+export const API_URL = 'https://dogsapi.origamid.dev/json';
 
 export function TOKEN_POST(body) {
     return {
@@ -8,9 +8,9 @@ export function TOKEN_POST(body) {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(body)
-        }
-    }
+            body: JSON.stringify(body),
+        },
+    };
 }
 
 export function TOKEN_VALIDATE_POST(token) {
@@ -19,10 +19,10 @@ export function TOKEN_VALIDATE_POST(token) {
         options: {
             method: 'POST',
             headers: {
-                Authorization: 'Bearer ' + token
-            }
-        }
-    }
+                Authorization: 'Bearer ' + token,
+            },
+        },
+    };
 }
 
 export function USER_GET(token) {
@@ -31,11 +31,10 @@ export function USER_GET(token) {
         options: {
             method: 'GET',
             headers: {
-                Authorization: 'Bearer ' + token
+                Authorization: 'Bearer ' + token,
             },
-        }
-    }
-
+        },
+    };
 }
 
 export function USER_POST(body) {
@@ -46,10 +45,9 @@ export function USER_POST(body) {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(body)
-        }
-    }
-
+            body: JSON.stringify(body),
+        },
+    };
 }
 
 export function PHOTO_POST(formData, token) {
@@ -58,11 +56,11 @@ export function PHOTO_POST(formData, token) {
         options: {
             method: 'POST',
             headers: {
-                Authorization: 'Bearer ' + token
+                Authorization: 'Bearer ' + token,
             },
-            body: formData
-        }
-    }
+            body: formData,
+        },
+    };
 }
 
 export function PHOTOS_GET({ page, total, user }) {
@@ -70,7 +68,7 @@ export function PHOTOS_GET({ page, total, user }) {
         url: `${API_URL}/api/photo/?_page=${page}&_total=${total}&_user=${user}`,
         options: {
             method: 'GET',
-            cache: 'no-store'
+            cache: 'no-store',
         },
     };
 }
@@ -80,7 +78,7 @@ export function PHOTO_GET(id) {
         url: `${API_URL}/api/photo/${id}`,
         options: {
             method: 'GET',
-            cache: 'no-store'
+            cache: 'no-store',
         },
     };
 }
@@ -92,9 +90,9 @@ export function COMMENT_POST(id, body) {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                Authorization: 'Bearer ' + window.localStorage.getItem('token')
+                Authorization: 'Bearer ' + window.localStorage.getItem('token'),
             },
-            body: JSON.stringify(body)
+            body: JSON.stringify(body),
         },
     };
 }
@@ -105,8 +103,7 @@ export function PHOTO_DELETE(id) {
         options: {
             method: 'DELETE',
             headers: {
-                'Content-Type': 'application/json',
-                Authorization: 'Bearer ' + window.localStorage.getItem('token')
+                Authorization: 'Bearer ' + window.localStorage.getItem('token'),
             },
         },
     };
